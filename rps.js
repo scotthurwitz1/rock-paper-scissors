fc532fcfbba11898501b942a885aee0adb3b7c06
Scotthurwitz1 <hurwitzscott@gmail.com>
2022-06-22 14:21:14 -0400

    create player input

--- rps.js
+++ rps.js
@@ -1,8 +1,15 @@
-let rps = ['Rock', "Paper", "Scissors"];
+let rps = ['rock', "paper", "scissors"];
 
 function computerPlay() {
     let cp = rps[Math.floor(Math.random()*rps.length)]
     return cp
+}
+
+function fn2() {
+    let pp = document.getElementById("player1").value;
+    if (rps.includes(pp) == false) {
+        alert("Try again");
+    }
 }
 
 computerPlay()
