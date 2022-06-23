fa3590963fe5d62cc8f666778e420ef06f15c4e6
Scotthurwitz1 <hurwitzscott@gmail.com>
2022-06-22 20:49:16 -0400

    Add scorekeeping and five round format

--- rps.js
+++ rps.js
@@ -21,25 +21,42 @@ function playRound() {
     if (playerSelection===computerSelection) {
         alert("Draw!")
         playRound()
-    } else if (playerSelection === "paper") {
+    } 
+    else if (playerSelection === "paper") {
         if (computerSelection === "rock") {
-            console.log("Player Wins")
-        } else {console.log("Computer wins")}
+            console.log("Player Wins");
+            playerScore += 1;
+        } else { console.log("Computer wins");
+            computerScore += 1;}
     }
     else if (playerSelection === "rock") {
         if (computerSelection === "scissors") {
-            console.log("Player Wins")
-        } else {console.log("Computer wins")}
+            console.log("Player Wins");
+            playerScore += 1;
+        } else {console.log("Computer wins")
+            computerScore += 1;}
     }
     else if (playerSelection === "scissors") {
         if (computerSelection === "paper") {
-            console.log("Player Wins")
-        } else {console.log("Computer wins")}
+            console.log("Player Wins");
+            playerScore += 1;
+        } else {console.log("Computer wins")
+            computerScore += 1;}
     }
 }
 
+let playerScore = 0;
+let computerScore = 0;
 
-playRound();
+for (let i=0; i<5; i++){
+    playRound()
+}
+
+console.log("Final Score:")
+console.log("Computer: "+computerScore)
+console.log("Player: "+playerScore)
+
+
 
 
 
